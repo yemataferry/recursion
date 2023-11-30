7e0aef8f1a1d816c1de9a89d7827cfbc3c0e95af
Yemata Ferry <yemataferry@gmail.com>
2023-11-30 22:36:08 +0100

    assignment completed

--- assetes/scripts/countChar.js
+++ assetes/scripts/countChar.js
@@ -1,7 +1,3 @@
-// write a fxn called countChar that behaves like countBs expect it takes a second arg that indicates the character that is to be counted
-// countChar("car", "c") => 1
-// countChar("capricious", "c") => 2
-
 let count = 0
 function countChar (str, charact) {
   for (let i = 0; i <= str.length - 1; i++) {
@@ -9,8 +5,8 @@ function countChar (str, charact) {
       count += 1
     }
   }
-  return count
+  return count 
 }
 
 console.log(countChar('BanaBBasaB', 'B'))
-console.log(countChar('AnAphilAcTic', 'A'))
+// console.log(countChar('AnAphilAcTic', 'A'))
